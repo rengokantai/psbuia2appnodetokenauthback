44f1e32115d907b1c02842f4ab13266273df45b1
Hernan Y.Ke <yuriqiao@gmail.com>
2017-11-08 18:59:44 -0500

    npm install --save cors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express')
+var cors = require('cors')
 var app = express()
 
 var posts = [
   {message:'ke'}
 ]
 
+app.use(cors())
+
 app.get('/posts',(req,res)=>{
   res.send(posts)
 })
-
+import { ApiService} from './api.service';
 
 app.listen(3000)
