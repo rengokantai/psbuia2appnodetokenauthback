08c794fd3cc11a750deb1280e24b297264ec3d41
Hernan Y.Ke <yuriqiao@gmail.com>
2017-11-09 10:04:21 -0500

    body-parser need to be json

--- server.js
+++ server.js
@@ -11,19 +11,22 @@ var posts = [
 ]
 
 app.use(cors())
-app.use(bodyParser.text())
+app.use(bodyParser.json())
 
 app.get('/posts',(req,res)=>{
   res.send(posts)
 })
 
-app.get('/register',(req,res)=>{
+app.post('/register',(req,res)=>{
   var userData = req.body
   var user = new User(userData);
   user.save((err,result)=>{
-    if(err){}
+    if(err){
+       console.log('error')
+    }
+    res.sendStatus(200)
   })
-  res.sendStatus(200)
+  
 })
 
 mongoose.connect('mongodb://root:root@ds155325.mlab.com:55325/psbuia2appnodetokenauthback',{useMongoClient:true},(err)=>{
